1e60e73ce1e9c1d35a4df098439028cb25ca851a
femmyte <sanyaoluadefemi@gmail.com>
2022-10-18 09:02:26 +0100

    added the message and user reducers to the store

--- src/store.js
+++ src/store.js
@@ -4,12 +4,7 @@ import messageSlice from "./action/message";
 export const store = configureStore({
   reducer: {
     users: userSlice,
-    message: messageSlice,
+    messages: messageSlice,
   },
   devTools: true,
 });
-
-// Infer the `RootState` and `AppDispatch` types from the store itself
-// export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-// export type AppDispatch = typeof store.dispatch;
