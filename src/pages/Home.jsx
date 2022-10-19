9ecd798b19b7268cfda0f581f23ac8a7bbcbae23
femmyte <sanyaoluadefemi@gmail.com>
2022-10-19 09:02:16 +0100

    Fetch User from localStorage
    - Changed the user identifier from the store to the localStorage in order to get the user that is currently stored in the localStorage
    - Used tailwindcss framework to style the Home component and removing the inline style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,25 @@ import { useSelector } from "react-redux";
 import Message from "../components/Message";
 import UserRegAlert from "../components/UserRegAlert";
 const Home = () => {
-  //   const [id, setId] = useState("");
-  //   const [nameInput, setNameInput] = useState("");
-  const user = useSelector((state) => state.users);
-  return user.value !== "" ? (
-    <div>
-      <Message />
-    </div>
-  ) : (
-    <UserRegAlert />
-  );
+  // const user = useSelector((state) => state.message);
+  // console.log(user);
+  const handleFetchUser = () => {
+    return JSON.parse(localStorage.getItem("user"));
+  };
+  window.addEventListener("storage", handleFetchUser);
+  let user = handleFetchUser();
+  const handleFetch = () => {
+    return JSON.parse(localStorage.getItem("item"));
+  };
+  window.addEventListener("storage", handleFetch);
+  if (user)
+    return (
+      <div className="w-4/5 md:w-3/5 mx-auto rounded-sm shadow border border-gray-200 p-[20px] overflow-hidden h-[95vh]">
+        <Message />
+      </div>
+    );
+
+  return <UserRegAlert />;
 };
 
 export default Home;
