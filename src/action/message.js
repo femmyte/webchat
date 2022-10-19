c63f3f4cc145251fc4299c248df188f6a8199b91
femmyte <sanyaoluadefemi@gmail.com>
2022-10-19 06:16:11 +0100

    Added dispatch event listener:
    This enables the app to send a message to the app when a new item is been stored in the local storage

--- src/action/message.js
+++ src/action/message.js
@@ -1,7 +1,8 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 import dateFormat, { masks } from "dateformat";
 const now = new Date();
 let id = now.getTime().toString();
+
 const userMessageSlice = createSlice({
   name: "messages",
   initialState: JSON.parse(localStorage.getItem("item")) || {
@@ -11,14 +12,15 @@ const userMessageSlice = createSlice({
   reducers: {
     addMessage: (state, action) => {
       state.message.push(action.payload, dateFormat(now, "longTime"));
-      state.id += id;
-      localStorage.setItem(
+      state.id += 1;
+      window.localStorage.setItem(
         "item",
         JSON.stringify({
           message: state.message,
           id: state.id,
         })
       );
+      window.dispatchEvent(new Event("storage"));
     },
   },
 });
