39bcfd2ccfcc0eb19f35f1527f43c83852b8b371
femmyte <sanyaoluadefemi@gmail.com>
2022-10-19 06:32:32 +0100

    Refactored the component:
    Get the message and the user directly from the localStorage whenever a new message is been sent
    Checked the browser visibility to determine if a new tab was opened and makes the browser to reload to make the first user message appear

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,18 +1,40 @@
-import React, { useStatem, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CreateMessage from "./CreateMessage";
+import { addMessage } from "../action/message";
 
 const Message = () => {
-  const user = useSelector((state) => state.users);
-  const message = useSelector((state) => state.messages);
-  message.message.map((item) => console.log(item));
+  // const user = useSelector((state) => state.users);
+  let message = useSelector((state) => state.messages);
+  const dispatch = useDispatch();
+  const [msg, setMsg] = useState("");
+  const handleFetch = () => {
+    return JSON.parse(localStorage.getItem("item"));
+  };
+  let item = handleFetch();
+  const handleFetchUser = () => {
+    return JSON.parse(localStorage.getItem("user"));
+  };
+  window.addEventListener("storage", handleFetchUser);
+  let user = handleFetchUser();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addMessage(msg));
+    setMsg("");
+  };
+
+  document.addEventListener("visibilitychange", () => {
+    console.log("changed");
+    window.location.reload();
+  });
 
   return (
     <div>
-      <p>{user.value}</p>
-      {message.message.map((item) => (
-        <p>{item}</p>
-      ))}
+      {item && item.message.map((it) => <p key={item.id}>{it}</p>)}
+      {}
+      {/* {user && <p>{user.user[0]}</p>} */}
+      {/* {user && user.user.map((it) => <p key={user.userId}>{it}</p>)} */}
       <p>create New Message</p>
       <CreateMessage />
     </div>
