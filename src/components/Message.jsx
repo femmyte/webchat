6a0bb08dddee5dcfe5c476e892e17c4af4ce475f
femmyte <sanyaoluadefemi@gmail.com>
2022-10-19 15:23:02 +0100

    Display the user name on the screen

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,48 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CreateMessage from "./CreateMessage";
-import { addMessage } from "../action/message";
 
-const Message = () => {
-  // const user = useSelector((state) => state.users);
+const Message = ({ user, item, userId }) => {
   let message = useSelector((state) => state.messages);
   const dispatch = useDispatch();
-  const [msg, setMsg] = useState("");
-  const handleFetch = () => {
-    return JSON.parse(localStorage.getItem("item"));
-  };
-  let item = handleFetch();
-  const handleFetchUser = () => {
-    return JSON.parse(localStorage.getItem("user"));
-  };
-  window.addEventListener("storage", handleFetchUser);
-  let user = handleFetchUser();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(addMessage(msg));
-    setMsg("");
-  };
-
+  // console.log("user >>", user);
+  // console.log("userId >>", userId);
+  // console.log("message >>", item);
   document.addEventListener("visibilitychange", () => {
-    console.log("changed");
     window.location.reload();
   });
-  item.message.map((it) => console.log(it.m));
-  // console.log(item.message);
+
   return (
     <div className="flex flex-col h-full overflow-auto no-scrollbar relative">
       {item &&
-        item.message.map((it) => (
-          <div key={item.id} className="block md:flex my-4 w-4/5 md:w-3/5">
-            <p className="bg-[#D9D9D9] py-2 px-4 rounded-3xl">{it.m}</p>
-            <p className="t text-xs mt-2">{it.time} </p>
-          </div>
-        ))}
+        item.message.map((it) => {
+          if (it.userId === userId) {
+            console.log(it.m);
+            return (
+              <div key={item.id} className="block md:flex my-4 w-4/5 md:w-3/5">
+                <p className="bg-[#D9D9D9] py-2 px-4 rounded-3xl">{it.m}</p>
+                <p className="t text-xs mt-2">{it.time} </p>
+              </div>
+            );
+          } else {
+            return (
+              <div key={item.id} className="block md:flex my-4 w-4/5 md:w-3/5">
+                <p className="bg-[#CBF49E] py-2 px-4 rounded-3xl">{it.m}</p>
+                <p className="t text-xs mt-2">{it.time} </p>
+              </div>
+            );
+          }
+        })}
       {}
-      {/* {user && <p>{user.user[0]}</p>} */}
-      {/* {user && user.user.map((it) => <p key={user.userId}>{it}</p>)} */}
-      <div className="sticky bg-white bottom-2 w-full ">
+      {user && user.user.map((it) => <p key={user.userId}>{it.user}</p>)}
+      <div className="sticky top-[90%] bg-white bottom-2 w-full ">
         <CreateMessage />
       </div>
     </div>
