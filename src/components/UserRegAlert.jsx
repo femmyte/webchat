c36720118b833775d96d1bc7e6b868f8dfbe5194
femmyte <sanyaoluadefemi@gmail.com>
2022-10-19 06:21:06 +0100

    Refactored the component:
    Set the input value dispatched only id the user submit the form

--- src/components/UserRegAlert.jsx
+++ src/components/UserRegAlert.jsx
@@ -3,26 +3,29 @@ import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../action/user";
 
 const UserRegAlert = () => {
-  const [id, setId] = useState("");
-  const user = useSelector((state) => state.users);
-
+  // const [id, setId] = useState("");
+  // const user = useSelector((state) => state.users);
   const dispatch = useDispatch();
+  const [addNewUser, setAddNewUser] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!user) {
-      return alert("Name can't be empty");
-    }
-    setId(user);
-    console.log(user);
+    // if (!user) {
+    //   return alert("Name can't be empty");
+    // }
+    dispatch(addUser(addNewUser));
+    setAddNewUser("");
   };
+  // dispatch(addUser(e.target.value.trim())
   return (
     <div style={{ textAlign: "center", margin: "30vh auto", width: "70%" }}>
       <form onSubmit={(event) => handleSubmit(event)}>
         <input
           id="name"
-          onChange={(e) => dispatch(addUser(e.target.value.trim()))}
+          onChange={(e) => setAddNewUser(e.target.value)}
           required
           placeholder="What is your name .."
+          value={addNewUser}
         />
 
         <button className="mt-[15px]" type="submit">
