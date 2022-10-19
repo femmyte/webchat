7f7a13543f03a5b6c225102e02965874fbd03f24
femmyte <sanyaoluadefemi@gmail.com>
2022-10-19 06:16:44 +0100

    Added dispatch event listener:
    This enables the app to send a message to the app when a new user is been stored in the local storage

--- src/action/user.js
+++ src/action/user.js
@@ -1,16 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = JSON.parse(localStorage.getItem("item")) || {
-  value: "",
-};
-
-export const userSlice = createSlice({
+const userSlice = createSlice({
   name: "users",
-  initialState,
+  initialState: JSON.parse(localStorage.getItem("user")) || {
+    user: [],
+    userId: 1,
+  },
   reducers: {
     addUser: (state, action) => {
-      state.value += action.payload;
-      localStorage.setItem("user", JSON.stringify(state.value));
+      state.user.push(action.payload);
+      state.userId += 1;
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          user: state.user,
+          userId: state.userId,
+        })
+      );
+      window.dispatchEvent(new Event("storage"));
     },
   },
 });
